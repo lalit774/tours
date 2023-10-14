3ea037e878dececb95f01a2d835964f7731d908d
lalit kumar <lalit.kumar@socialinnovation.ca>
2023-10-14 15:31:51 -0400

    tours project

---
+++ src/App.jsx
@@ -0,0 +1,63 @@
+import { useEffect, useState } from 'react'
+import { Tours } from './Tours'
+import Loading from './Loading'
+
+const url = 'https://course-api.com/react-tours-project'
+
+const App = () => {
+  const [isLoading, setIsLoading] = useState(true)
+  const [tours, setTours] = useState([])
+
+  const removeTour = (id) => {
+    const newTours = tours.filter((tour) => tour.id !== id)
+    setTours(newTours)
+  }
+
+  const fetchTours = async () => {
+    setIsLoading(true)
+    try {
+      const response = await fetch(url)
+      const data = await response.json()
+      setTours(data)
+    } catch (error) {
+      console.log(error)
+    }
+    setIsLoading(false)
+  }
+
+  useEffect(() => {
+    fetchTours()
+  }, [])
+  if (isLoading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
+  if (tours.length === 0) {
+    return (
+      <main>
+        <div className="title">
+          <h2>No tour left</h2>
+          <button
+            type="button"
+            className="btn"
+            style={{ marginTop: '2rem' }}
+            onClick={() => {
+              fetchTours()
+            }}
+          >
+            refresh
+          </button>
+        </div>
+      </main>
+    )
+  }
+  return (
+    <main>
+      <Tours tours={tours} removeTour={removeTour} />
+    </main>
+  )
+}
+export default App
